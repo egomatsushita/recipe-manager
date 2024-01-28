af7757e6f27f6cce4452dec0d3f5a526a6b491e8
Eduardo Go <eduardo.matsushita@autocase.com>
2024-01-28 18:19:47 -0500

    [RM-3] feat: Set up home page and about and contact template

---
+++ app/about/page.tsx
@@ -0,0 +1,22 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1 className='text-6xl mb-8'>About</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, repellat distinctio illo soluta natus
+        atque vitae beatae laudantium fugit iste quibusdam cum consequuntur ipsam. Minus pariatur velit quam commodi
+        quod.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, repellat distinctio illo soluta natus
+        atque vitae beatae laudantium fugit iste quibusdam cum consequuntur ipsam. Minus pariatur velit quam commodi
+        quod.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, repellat distinctio illo soluta natus
+        atque vitae beatae laudantium fugit iste quibusdam cum consequuntur ipsam. Minus pariatur velit quam commodi
+        quod.
+      </p>
+    </div>
+  )
+}
